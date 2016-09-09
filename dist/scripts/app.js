22ec055409ba97e1468f469b8369db1644f795c0
Dustin <dustinwaggoner@comcast.net>
2016-09-09 13:13:10 -0700

    start button starts countdown, stopping resets dafult number back to normal

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,5 +1,17 @@
 (function() {
+  function config($locationProvider) {
+    $locationProvider
+        .html5Mode({ //hashBang URLs are disabled
+        enabled: true,
+        requireBase: false
+    });
 
-angular
-    .module('blocTime', ['firebase', 'ui.router']);
+
+  };
+
+
+
+    angular
+        .module('blocTime', ['firebase', 'ui.router'])
+        .config(config);
 })();
